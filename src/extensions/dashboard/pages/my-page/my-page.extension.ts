783dabe9fae00a27c0bd2e566a7440bd71484329
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-04-16 10:19:12 +0300

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'f1aa5565-6dce-41be-9cf8-be3711ca1cd9',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
